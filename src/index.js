e2f569af36bc4f31dd2246a47bf4b802859875f1
SahuPrachi <prachisahu998@gmail.com>
2023-06-11 20:11:32 +0530

    feat: Replace AuthContext file inside folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { makeServer } from "./server";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import { makeServer } from "./server";
+import { AuthProvider } from "./context/authContext/AuthContext";
 import { DataProvider } from "./context/dataContext/DataContext";
+import "./index.css";
 
 makeServer();
 
