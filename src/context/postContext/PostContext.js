fd5a4ee794c297c3bacf9221d5a7d7572272e4b7
SahuPrachi <prachisahu998@gmail.com>
2023-06-13 23:08:22 +0530

    feat: Post data fetched

--- src/context/postContext/PostContext.js
+++ src/context/postContext/PostContext.js
@@ -1,9 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const PostContext = createContext();
 
 export const PostProvider = ({ children }) => {
-  return <PostContext.Provider value={{}}>{children}</PostContext.Provider>;
+  const [postData, setPostData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getPostData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/posts");
+      const dataFetched = await res.json();
+
+      if (res.status === 200) {
+        setPostData(dataFetched?.posts);
+        setTimeout(() => setLoading(false), 1000);
+      }
+    } catch (e) {
+      console.log("Error:", e);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  return (
+    <PostContext.Provider value={{ postData }}>{children}</PostContext.Provider>
+  );
 };
 
 export const usePost = () => useContext(PostContext);
