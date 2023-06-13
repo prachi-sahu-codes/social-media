17a6de3efa3e5a719f37c36914a6fbcf1dd98c2b
SahuPrachi <prachisahu998@gmail.com>
2023-06-13 22:20:21 +0530

    feat: Added post data

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -1,34 +1,122 @@
 import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 
-/**
- * Posts can be added here.
- * You can add default posts of your wish with different attributes
- * */
-
 export const posts = [
   {
-    _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    _id: "skdjeo903_kds3_ksd8s_ksd83",
+    content: "Capturing the beauty of nature.",
+    image: "art1.jpg",
     likes: {
-      likeCount: 0,
+      likeCount: 82,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
-    createdAt: formatDate(),
+    comments: [
+      {
+        _id: "ksjd93_ksjd29_ksdjiw9",
+        username: "ArtEnthusiast",
+        text: "Amazing artwork!",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    username: "Artist1",
+    createdAt: "2023-06-01T10:55:01+18:30",
     updatedAt: formatDate(),
   },
+
+  {
+    _id: "kdiw8s3_ksd9s_ksd38_ksd2",
+    content: "Exploring abstract expressions.",
+    image: "art2.jpg",
+    likes: {
+      likeCount: 65,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "ArtisticSoul",
+    createdAt: "2023-06-03T18:20:10+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "ksd2k3_ksdo29_2ksd_ksdj2",
+    content: "A masterpiece in progress.",
+    image: "art3.jpg",
+    likes: {
+      likeCount: 112,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "kjnd3832_jw93j_k3js",
+        username: "ArtAdmirer",
+        text: "Incredible work!",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    username: "CreativeArtist",
+    createdAt: "2023-06-05T12:45:30+02:00",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "ksdoi30_2kd3i9_kd3i",
+    content: "Finding inspiration in nature's colors.",
+    image: "art4.jpg",
+    likes: {
+      likeCount: 48,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "ArtExplorer",
+    createdAt: "2023-06-08T09:10:20+08:00",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "sod9s_23ksd8_ksdjw",
+    content: "Expressing emotions through sculpture.",
+    image: "art5.jpg",
+    likes: {
+      likeCount: 95,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "kdoe9d3_ksd83k_ksd32",
+        username: "ArtLover123",
+        text: "Great job!",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    username: "CreativeMind",
+    createdAt: "2023-06-10T16:35:55+10:00",
+    updatedAt: formatDate(),
+  },
+
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Enjoying the art festival!",
+    image: "art6.jpg",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
+    comments: [],
     username: "shubhamsoni",
     createdAt: formatDate(),
     updatedAt: formatDate(),
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Navbar } from "../../components/navbar/Navbar";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="mt-12"></div>
+    </div>
+  );
 };
