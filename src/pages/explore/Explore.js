ecf318fa425b169c5125ba89b367a26685f1c21d
SahuPrachi <prachisahu998@gmail.com>
2023-06-15 21:27:56 +0530

    fix: Shifted post data to explore page

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { usePost } from "../../context/postContext/PostContext";
 
 export const Explore = () => {
-  return <div className="w-full py-7 px-6">Explore</div>;
+  const { postData } = usePost();
+
+  return (
+    <div className="w-full py-7 px-6 h-calc-nav overflow-x-hidden overflow-y-scroll">
+      <ul>
+        {postData.map((post) => (
+          <li>{post.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
