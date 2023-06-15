e0a3e4eede23e28bce4d837793c5781aaf8602a5
SahuPrachi <prachisahu998@gmail.com>
2023-06-16 04:10:50 +0530

    feat: data added

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -5,17 +5,23 @@ export const posts = [
     _id: "ksd2k3_ksdo29_2ksd_ksdj2",
     content:
       "A masterpiece in progress. Immersing in the dreamlike landscapes and ethereal brushstrokes of impressionist masterpieces, where art captures fleeting moments.",
-    image: "https://i.imgur.com/Rytba1u.png",
-    contentImage: "https://i.imgur.com/5EZlfZm.png",
+    profileImage: "https://i.imgur.com/Rytba1u.png",
+
     likes: {
       likeCount: 112,
-      likedBy: [],
+      likedBy: [
+        {
+          fullName: "William Hughes",
+          username: "hugesSiliam",
+          profileImage: "https://i.imgur.com/dGLRy0M.png",
+        },
+      ],
       dislikedBy: [],
     },
     comments: [
       {
         _id: "ksdoi30_2kd3i9_kd3i",
-        image: "https://i.imgur.com/dGLRy0M.png",
+        profileImage: "https://i.imgur.com/dGLRy0M.png",
         username: "hugesSiliam",
         text: "Eagerly waiting!",
         votes: {
@@ -26,7 +32,7 @@ export const posts = [
     ],
     fullName: "Noah Thompson",
     username: "Noah_Thompson",
-    occupation: "Renaissance Polymath",
+    bio: "Renaissance Polymath",
     createdAt: "2023-06-05T12:45:30+02:00",
     updatedAt: formatDate(),
   },
@@ -35,7 +41,7 @@ export const posts = [
     _id: "skdjeo903_kds3_ksd8s_ksd83",
     content:
       "Just had an incredible day exploring the world of art! 🎨 Enlarged flowers, desert landscapes—every brushstroke was a journey into new realms of creativity. Share your recent art adventures with me! What have you been exploring in the art world? Let's ignite our imagination together!",
-    image: "https://i.imgur.com/07fk2I9.png",
+    profileImage: "https://i.imgur.com/07fk2I9.png",
     likes: {
       likeCount: 82,
       likedBy: [],
@@ -45,7 +51,7 @@ export const posts = [
       {
         _id: "kdiw8s3_ksd9s_ksd38_ksd2",
         username: "carterAmii",
-        image: "https://i.imgur.com/hRwELzq.png",
+        profileImage: "https://i.imgur.com/hRwELzq.png",
         text: "I have been finding inspiration in nature's colors.",
         votes: {
           upvotedBy: [],
@@ -55,7 +61,7 @@ export const posts = [
     ],
     fullName: "Liam Parker",
     username: "_liam_parker_",
-    occupation: "Post-Impressionist Painter",
+    bio: "Post-Impressionist Painter",
     createdAt: "2023-06-01T10:55:01+18:30",
     updatedAt: formatDate(),
   },
@@ -64,7 +70,7 @@ export const posts = [
     _id: "kdiw8s3_ksd9s_ksd38_ksd2",
     content:
       "Diving into the realm of abstract art, where colors, shapes, and textures intertwine to evoke emotions and spark imagination.",
-    image: "https://i.imgur.com/hRwELzq.png",
+    profileImage: "https://i.imgur.com/hRwELzq.png",
     likes: {
       likeCount: 65,
       likedBy: [],
@@ -73,7 +79,7 @@ export const posts = [
     comments: [],
     fullName: "Amelia Carter",
     username: "carterAmii",
-    occupation: "Surrealist Painter",
+    bio: "Surrealist Painter",
     createdAt: "2023-06-03T18:20:10+05:30",
     updatedAt: formatDate(),
   },
@@ -82,7 +88,7 @@ export const posts = [
     _id: "ksdoi30_2kd3i9_kd3i",
     content:
       "Currently immersed in an art festival bursting with creativity and inspiration! 🎉🎭 From captivating sculptures to thought-provoking installations, I can't get enough. What art festivals or events have caught your attention lately? Let's share our favorite discoveries and keep the artistry flowing!",
-    image: "https://i.imgur.com/dGLRy0M.png",
+    profileImage: "https://i.imgur.com/dGLRy0M.png",
     likes: {
       likeCount: 48,
       likedBy: [],
@@ -91,7 +97,7 @@ export const posts = [
     comments: [],
     fullName: "William Hughes",
     username: "hugesSiliam",
-    occupation: "Cubist Painter, Sculptor",
+    bio: "Cubist Painter, Sculptor",
     createdAt: "2023-06-08T09:10:20+08:00",
     updatedAt: formatDate(),
   },
@@ -99,7 +105,7 @@ export const posts = [
   {
     _id: "sod9s_23ksd8_ksdjw",
     content: "Expressing emotions through sculpture.",
-    image: "https://i.imgur.com/RcLQNaA.png",
+    profileImage: "https://i.imgur.com/RcLQNaA.png",
     likes: {
       likeCount: 95,
       likedBy: [],
@@ -109,7 +115,7 @@ export const posts = [
       {
         _id: "kdoe9d3_ksd83k_ksd32",
         username: "claudemonet",
-        image: "https://i.imgur.com/usR4cPn.png",
+        profileImage: "https://i.imgur.com/usR4cPn.png",
         text: "Great job!",
         votes: {
           upvotedBy: [],
@@ -119,7 +125,7 @@ export const posts = [
     ],
     fullName: "Georgia O'Keeffe",
     username: "georgiaokeeffe",
-    occupation: "Modernist Painter",
+    bio: "Modernist Painter",
     createdAt: "2023-06-10T16:35:55+10:00",
     updatedAt: formatDate(),
   },
@@ -127,16 +133,16 @@ export const posts = [
   {
     _id: "kdoe9d3_ksd83k_ksd32",
     content: "Immersed in Nature's Palette, Light, color, fleeting moments.",
-    image: "https://i.imgur.com/usR4cPn.png",
+    profileImage: "https://i.imgur.com/usR4cPn.png",
     likes: {
-      likeCount: 0,
+      likeCount: 20,
       likedBy: [],
       dislikedBy: [],
     },
     comments: [],
     fullName: "Claude Monet",
     username: "claudemonet",
-    occupation: "Impressionist Painter",
+    bio: "Impressionist Painter",
     createdAt: "2022-12-25T18:45:10-03:00",
     updatedAt: formatDate(),
   },
@@ -145,16 +151,16 @@ export const posts = [
     _id: "kdoe9d3_ksd83k_ksd32",
     content:
       "Witnessing the vibrant hues of flowers in full bloom. Nature's color palette is a true work of art!",
-    image: "https://i.imgur.com/3koLVx8.png",
+    profileImage: "https://i.imgur.com/3koLVx8.png",
     likes: {
-      likeCount: 0,
+      likeCount: 60,
       likedBy: [],
       dislikedBy: [],
     },
     comments: [],
     fullName: "Jenny Felicitas",
     username: "JoyfulJenny",
-    occupation: "Impressionist Painter",
+    bio: "Impressionist Painter",
     createdAt: "2022-09-15T08:30:45+05:00",
     updatedAt: formatDate(),
   },
@@ -162,7 +168,7 @@ export const posts = [
     _id: "skdjeo903_kds3_ksd8s_ksd93",
     content:
       "Wandering through vibrant streets adorned with captivating murals and graffiti, where creativity spills beyond gallery walls.",
-    image: "https://i.imgur.com/rq3yabN.png",
+    profileImage: "https://i.imgur.com/rq3yabN.png",
     likes: {
       likeCount: 82,
       likedBy: [],
@@ -172,7 +178,7 @@ export const posts = [
       {
         _id: "kdiw8s3_ksd9s_ksd38_ksd2",
         username: "carterAmii",
-        image: "https://i.imgur.com/hRwELzq.png",
+        profileImage: "https://i.imgur.com/hRwELzq.png",
         text: "Inspiring!",
         votes: {
           upvotedBy: [],
@@ -182,7 +188,7 @@ export const posts = [
     ],
     fullName: "Vincent van Gogh",
     username: "vangogh",
-    occupation: "Post-Impressionist Painter",
+    bio: "Post-Impressionist Painter",
     createdAt: "2023-06-01T10:55:01+18:30",
     updatedAt: formatDate(),
   },
--- src/components/postCard/PostCard.js
+++ src/components/postCard/PostCard.js
@@ -3,8 +3,13 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaRegHeart, FaHeart, FaRegComment } from "react-icons/fa";
 import { HiOutlineShare, HiOutlineBookmark, HiBookmark } from "react-icons/hi";
 import moment from "moment";
+import { useAuth } from "../../context/authContext/AuthContext";
 
 export const PostCard = ({ post }) => {
+  const { loggedUser } = useAuth();
+  console.log("loggedUser", loggedUser);
+  console.log("post", post);
+  const checkUser = loggedUser.username !== post.username;
   var formattedDate = moment(post?.createdAt).format("ddd MMM DD YYYY");
 
   return (
@@ -12,17 +17,21 @@ export const PostCard = ({ post }) => {
       <div className="flex justify-between items-center">
         <div className="flex gap-3 items-center">
           <img
-            src={post.image}
+            src={post.profileImage}
             alt="profile pic"
-            className="w-12 h-12 rounded-full border-2 border-solid border-primary"
+            className="w-12 h-12 rounded-full border-2 border-solid border-primary cursor-pointer"
           />
           <div>
-            <p className="font-semibold">{post.username}</p>
+            <p className="font-semibold cursor-pointer">{post.username}</p>
             <p className="text-xs text-gray">{formattedDate}</p>
           </div>
         </div>
 
-        <BsThreeDotsVertical className="text-xl" />
+        {checkUser ? (
+          ""
+        ) : (
+          <BsThreeDotsVertical className="text-xl cursor-pointer" />
+        )}
       </div>
 
       {post?.contentImage && (
@@ -35,24 +44,27 @@ export const PostCard = ({ post }) => {
         </div>
       )}
 
-      <p className="mt-5">{post.content}</p>
-      <div className="flex justify-between items-center pt-5 text-lg">
-        <div className="flex gap-2">
+      <p className="mt-6">{post.content}</p>
+
+      <div className="flex justify-between items-center mt-5 text-lg ">
+        <div className="flex gap-2 cursor-pointer">
           {true ? <FaRegHeart /> : <FaHeart />}
           <span className="text-sm">{post.likes.likeCount} Likes</span>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 cursor-pointer">
           <FaRegComment />
-          <span className="text-sm">Comment</span>
+          <span className="text-sm">
+            {post.comments.length === 0 ? "" : post.comments.length} Comment
+          </span>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 cursor-pointer">
           <HiOutlineShare />
           <span className="text-sm">Share</span>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 cursor-pointer">
           {true ? <HiOutlineBookmark /> : <HiBookmark />}
           <span className="text-sm">Bookmark</span>
         </div>
