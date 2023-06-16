084dcea196a8f8d7bf4826fd44a936f9ff849b35
SahuPrachi <prachisahu998@gmail.com>
2023-06-16 23:10:05 +0530

    feat: data added and updated

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -5,14 +5,14 @@ export const posts = [
     _id: "ksd2k3_ksdo29_2ksd_ksdj2",
     content:
       "A masterpiece in progress. Immersing in the dreamlike landscapes and ethereal brushstrokes of impressionist masterpieces, where art captures fleeting moments.",
+    contentImage: "",
     profileImage: "https://i.imgur.com/Rytba1u.png",
-
     likes: {
       likeCount: 112,
       likedBy: [
         {
           fullName: "William Hughes",
-          username: "hugesSiliam",
+          username: "HugesSiliam",
           profileImage: "https://i.imgur.com/dGLRy0M.png",
         },
       ],
@@ -20,12 +20,17 @@ export const posts = [
     },
     comments: [
       {
-        _id: "ksdoi30_2kd3i9_kd3i",
+        _id: "sJd4kE3oP9",
         profileImage: "https://i.imgur.com/dGLRy0M.png",
-        username: "hugesSiliam",
+        username: "HugesSiliam",
         text: "Eagerly waiting!",
+        createdAt: "2023-06-06T12:45:30+02:00",
         votes: {
-          upvotedBy: [],
+          upvotedBy: [
+            { username: "dwilson77" },
+            { username: "carterAmii" },
+            { username: "mikedavis456" },
+          ],
           downvotedBy: [],
         },
       },
@@ -38,9 +43,10 @@ export const posts = [
   },
 
   {
-    _id: "skdjeo903_kds3_ksd8s_ksd83",
+    _id: "skdjeo903_kds3_ksd8skd83",
     content:
       "Just had an incredible day exploring the world of art! 🎨 Enlarged flowers, desert landscapes—every brushstroke was a journey into new realms of creativity. Share your recent art adventures with me! What have you been exploring in the art world? Let's ignite our imagination together!",
+    contentImage: "",
     profileImage: "https://i.imgur.com/07fk2I9.png",
     likes: {
       likeCount: 82,
@@ -49,12 +55,17 @@ export const posts = [
     },
     comments: [
       {
-        _id: "kdiw8s3_ksd9s_ksd38_ksd2",
+        _id: "gYh4lOeD2",
         username: "carterAmii",
         profileImage: "https://i.imgur.com/hRwELzq.png",
         text: "I have been finding inspiration in nature's colors.",
+        createdAt: "2023-06-01T10:59:01+18:30",
         votes: {
-          upvotedBy: [],
+          upvotedBy: [
+            { username: "_liam_parker_" },
+            { username: "mikedavis456" },
+            { username: "dwilson77" },
+          ],
           downvotedBy: [],
         },
       },
@@ -85,9 +96,10 @@ export const posts = [
   },
 
   {
-    _id: "ksdoi30_2kd3i9_kd3i",
+    _id: "ksdoi30_2kd3i9_kd3ik3_ks",
     content:
       "Currently immersed in an art festival bursting with creativity and inspiration! 🎉🎭 From captivating sculptures to thought-provoking installations, I can't get enough. What art festivals or events have caught your attention lately? Let's share our favorite discoveries and keep the artistry flowing!",
+    contentImage: "",
     profileImage: "https://i.imgur.com/dGLRy0M.png",
     likes: {
       likeCount: 48,
@@ -96,15 +108,16 @@ export const posts = [
     },
     comments: [],
     fullName: "William Hughes",
-    username: "hugesSiliam",
+    username: "HugesSiliam",
     bio: "Cubist Painter, Sculptor",
     createdAt: "2023-06-08T09:10:20+08:00",
     updatedAt: formatDate(),
   },
 
   {
-    _id: "sod9s_23ksd8_ksdjw",
+    _id: "sod9s_2k3_ks3ksd8_ksdjw",
     content: "Expressing emotions through sculpture.",
+    contentImage: "",
     profileImage: "https://i.imgur.com/RcLQNaA.png",
     likes: {
       likeCount: 95,
@@ -113,12 +126,13 @@ export const posts = [
     },
     comments: [
       {
-        _id: "kdoe9d3_ksd83k_ksd32",
-        username: "claudemonet",
+        _id: "pQs8fKd39e",
+        username: "mikedavis456",
         profileImage: "https://i.imgur.com/usR4cPn.png",
         text: "Great job!",
+        createdAt: "2023-06-10T16:35:55+10:00",
         votes: {
-          upvotedBy: [],
+          upvotedBy: [{ username: "mikedavis456" }, { username: "dwilson77" }],
           downvotedBy: [],
         },
       },
@@ -131,8 +145,9 @@ export const posts = [
   },
 
   {
-    _id: "kdoe9d3_ksd83k_ksd32",
+    _id: "kdoe9d3k3_k_ksd83k_ksd32",
     content: "Immersed in Nature's Palette, Light, color, fleeting moments.",
+    contentImage: "",
     profileImage: "https://i.imgur.com/usR4cPn.png",
     likes: {
       likeCount: 20,
@@ -140,17 +155,18 @@ export const posts = [
       dislikedBy: [],
     },
     comments: [],
-    fullName: "Claude Monet",
-    username: "claudemonet",
+    fullName: "Michael Davis",
+    username: "mikedavis456",
     bio: "Impressionist Painter",
     createdAt: "2022-12-25T18:45:10-03:00",
     updatedAt: formatDate(),
   },
 
   {
-    _id: "kdoe9d3_ksd86k_ksd32",
+    _id: "k3_ks3ksdd3_ksd86k_ksd32",
     content:
       "Witnessing the vibrant hues of flowers in full bloom. Nature's color palette is a true work of art!",
+    contentImage: "",
     profileImage: "https://i.imgur.com/3koLVx8.png",
     likes: {
       likeCount: 60,
@@ -165,10 +181,11 @@ export const posts = [
     updatedAt: formatDate(),
   },
   {
-    _id: "skdjeo903_kds3_ksd8s_ksd93",
+    _id: "skdjeo903_kds3_ksd8ssd93",
     content:
       "Wandering through vibrant streets adorned with captivating murals and graffiti, where creativity spills beyond gallery walls.",
-    profileImage: "https://i.imgur.com/rq3yabN.png",
+    contentImage: "",
+    profileImage: "https://i.imgur.com/RUgHMHR.png",
     likes: {
       likeCount: 82,
       likedBy: [],
@@ -176,20 +193,20 @@ export const posts = [
     },
     comments: [
       {
-        _id: "kdiw8s3_ksd9s_ksd38_ksd2",
+        _id: "gYh4lOeD2",
         username: "carterAmii",
         profileImage: "https://i.imgur.com/hRwELzq.png",
         text: "Inspiring!",
         votes: {
-          upvotedBy: [],
+          upvotedBy: [{ username: "dwilson77" }],
           downvotedBy: [],
         },
       },
     ],
-    fullName: "Vincent van Gogh",
-    username: "vangogh",
-    bio: "Post-Impressionist Painter",
-    createdAt: "2023-06-01T10:55:01+18:30",
+    fullName: "David Wilson",
+    username: "dwilson77",
+    bio: "Pop Art Icon",
+    createdAt: "2023-06-02T10:55:01+18:30",
     updatedAt: formatDate(),
   },
 ];
--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
@@ -7,25 +7,246 @@ import { formatDate } from "../utils/authUtils";
 
 export const users = [
   {
-    _id: uuid(),
-    fullname: "Adarsh Balika",
-    username: "adarshbalika",
-    email: "adarshBalika@gmail.com",
-    password: "adarshBalika123",
-    profileImage: "https://i.imgur.com/hRwELzq.png",
+    _id: "jKs3o9dKl5",
+    fullname: "David Wilson",
+    username: "dwilson77",
+    email: "dwilson77@gmail.com",
+    password: "willll456@",
+    profileImage: "https://i.imgur.com/RUgHMHR.png",
+    profileBgImage: "",
     bio: "Pop Art Icon",
-    createdAt: formatDate(),
+    createdAt: "2023-05-02T10:55:01+18:30",
     updatedAt: formatDate(),
+    websiteLink: "https://furn.netlify.app/",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Noah Thompson",
+        username: "Noah_Thompson",
+        profileImage: "https://i.imgur.com/Rytba1u.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Jenny Felicitas",
+        username: "JoyfulJenny",
+        profileImage: "https://i.imgur.com/3koLVx8.png",
+      },
+    ],
   },
   {
-    _id: uuid(),
+    _id: "sLdk9e02s7",
     fullname: "Jenny Felicitas",
     username: "JoyfulJenny",
     email: "jenny123@gmail.com",
     password: "jenny123@",
     profileImage: "https://i.imgur.com/3koLVx8.png",
+    profileBgImage: "",
     bio: "Contemporary Artist",
-    createdAt: formatDate(),
+    createdAt: "2022-09-15T08:30:45+05:00",
     updatedAt: formatDate(),
+    websiteLink: "",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "David Wilson",
+        username: "dwilson77",
+        profileImage: "https://i.imgur.com/RUgHMHR.png",
+      },
+      {
+        fullname: "Amelia Carter",
+        username: "carterAmii",
+        profileImage: "https://i.imgur.com/hRwELzq.png",
+      },
+      {
+        fullname: "Michael Davis",
+        username: "mikedavis456",
+        profileImage: "https://i.imgur.com/usR4cPn.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Michael Davis",
+        username: "mikedavis456",
+        profileImage: "https://i.imgur.com/usR4cPn.png",
+      },
+    ],
+  },
+  {
+    _id: "pQs8fKd39e",
+    fullname: "Michael Davis",
+    username: "mikedavis456",
+    email: "davis4@gmail.com",
+    password: "mikedavis4@",
+    profileImage: "https://i.imgur.com/usR4cPn.png",
+    profileBgImage: "",
+    bio: "Impressionist Painter",
+    createdAt: "2022-12-25T18:45:10-03:00",
+    updatedAt: formatDate(),
+    websiteLink: "",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Jenny Felicitas",
+        username: "JoyfulJenny",
+        profileImage: "https://i.imgur.com/3koLVx8.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Jenny Felicitas",
+        username: "JoyfulJenny",
+        profileImage: "https://i.imgur.com/3koLVx8.png",
+      },
+      {
+        fullname: "Georgia O'Keeffe",
+        username: "georgiaokeeffe",
+        profileImage: "https://i.imgur.com/RcLQNaA.png",
+      },
+    ],
+  },
+  {
+    _id: "dOp9wE8sK4",
+    fullname: "Georgia O'Keeffe",
+    username: "georgiaokeeffe",
+    email: "georgia@gmail.com",
+    password: "georgia6@",
+    profileImage: "https://i.imgur.com/RcLQNaA.png",
+    profileBgImage: "",
+    bio: "Modernist Painter",
+    createdAt: "2023-05-10T16:35:55+10:00 ",
+    updatedAt: formatDate(),
+    websiteLink: "https://furn.netlify.app/",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Michael Davis",
+        username: "mikedavis456",
+        profileImage: "https://i.imgur.com/usR4cPn.png",
+      },
+    ],
+    following: [],
+  },
+  {
+    _id: "sJd4kE3oP9",
+    fullname: "William Hughes",
+    username: "HugesSiliam",
+    email: "hugesSiliam@gmail.com",
+    password: "Siliam3@",
+    profileImage: "https://i.imgur.com/dGLRy0M.png",
+    profileBgImage: "",
+    bio: "Cubist Painter, Sculptor",
+    createdAt: "2023-05-08T09:10:20+08:00",
+    updatedAt: formatDate(),
+    websiteLink: "",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Amelia Carter",
+        username: "carterAmii",
+        profileImage: "https://i.imgur.com/hRwELzq.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Amelia Carter",
+        username: "carterAmii",
+        profileImage: "https://i.imgur.com/hRwELzq.png",
+      },
+    ],
+  },
+  {
+    _id: "gYh4lOeD2",
+    fullname: "Amelia Carter",
+    username: "carterAmii",
+    email: "carterAmii@gmail.com",
+    password: "carterAmii56@",
+    profileImage: "https://i.imgur.com/hRwELzq.png",
+    profileBgImage: "",
+    bio: "Surrealist Painter",
+    createdAt: "2023-05-03T18:20:10+05:30",
+    updatedAt: formatDate(),
+    websiteLink: "https://furn.netlify.app/",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "William Hughes",
+        username: "HugesSiliam",
+        profileImage: "https://i.imgur.com/dGLRy0M.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "William Hughes",
+        username: "HugesSiliam",
+        profileImage: "https://i.imgur.com/dGLRy0M.png",
+      },
+      {
+        fullname: "Jenny Felicitas",
+        username: "JoyfulJenny",
+        profileImage: "https://i.imgur.com/3koLVx8.png",
+      },
+    ],
+  },
+  {
+    _id: "aSd7eL9qP0",
+    fullname: "Liam Parker",
+    username: "_liam_parker_",
+    email: "liam123@gmail.com",
+    password: "liam123@",
+    profileImage: "https://i.imgur.com/07fk2I9.png",
+    profileBgImage: "",
+    bio: "Post-Impressionist Painter",
+    createdAt: "2023-05-01T10:55:01+18:30",
+    updatedAt: formatDate(),
+    websiteLink: "",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Noah Thompson",
+        username: "Noah_Thompson",
+        profileImage: "https://i.imgur.com/Rytba1u.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Noah Thompson",
+        username: "Noah_Thompson",
+        profileImage: "https://i.imgur.com/Rytba1u.png",
+      },
+    ],
+  },
+  {
+    _id: "zXc8bN9mL0",
+    fullname: "Noah Thompson",
+    username: "Noah_Thompson",
+    email: "noah77@gmail.com",
+    password: "noah456@",
+    profileImage: "https://i.imgur.com/Rytba1u.png",
+    profileBgImage: "",
+    bio: "Renaissance Polymath",
+    createdAt: "2023-05-05T12:45:30+02:00",
+    updatedAt: formatDate(),
+    websiteLink: "https://furn.netlify.app/",
+    bookmarks: [],
+    followers: [
+      {
+        fullname: "Liam Parker",
+        username: "_liam_parker_",
+        profileImage: "https://i.imgur.com/07fk2I9.png",
+      },
+    ],
+    following: [
+      {
+        fullname: "Liam Parker",
+        username: "_liam_parker_",
+        profileImage: "https://i.imgur.com/07fk2I9.png",
+      },
+      {
+        fullname: "David Wilson",
+        username: "dwilson77",
+        profileImage: "https://i.imgur.com/RUgHMHR.png",
+      },
+    ],
   },
 ];
