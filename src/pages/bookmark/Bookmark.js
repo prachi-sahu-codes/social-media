7be5b21acd924585563b2d9f47f51585a588c4f8
SahuPrachi <prachisahu998@gmail.com>
2023-06-17 02:34:02 +0530

    style: Height added

--- src/pages/bookmark/Bookmark.js
+++ src/pages/bookmark/Bookmark.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Bookmark = () => {
-  return <div className="w-full py-7 px-6">Bookmark</div>;
+  return (
+    <div className="w-full py-7 px-6 h-calc-nav overflow-x-hidden overflow-y-scroll bg-slate-50">
+      Bookmark
+    </div>
+  );
 };
