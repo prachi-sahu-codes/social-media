d3f283959f5c3a6f6f35e3c281a6ee99cd8b70b6
SahuPrachi <prachisahu998@gmail.com>
2023-07-02 13:47:36 +0530

    style: font size changed

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -6,11 +6,11 @@ export const NotFound = () => {
   return (
     <div className="w-screen h-screen">
       <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-2/4 text-center">
-        <img src={notfound} alt="Not found" className="w-96 m-auto mb-2" />
-        <h1 className="font-semibold text-3xl p-2">
+        <img src={notfound} alt="Not found" className="w-96 m-auto mb-5" />
+        <h1 className="font-semibold text-lg sm500:text-2xl p-2">
           Looks like something is missing!!
         </h1>
-        <p className="text-xl p-2">Go back to explore page</p>
+        <p className="text-lg">Go back to explore page</p>
         <Link
           to="./explore"
           className="inline-block text-lg rounded-lg text-white bg-primary py-1 px-4 m-4"
