37b61f7a59b70b91205a681a9966e7e172179f87
SahuPrachi <prachisahu998@gmail.com>
2023-07-02 18:57:03 +0530

    feat: images added

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -3,9 +3,8 @@ import { formatDate } from "../utils/authUtils";
 export const posts = [
   {
     _id: "ksd2k3_ksdo29_2ksd_ksdj2",
-    content:
-      "A masterpiece in progress. Immersing in the dreamlike landscapes and ethereal brushstrokes of impressionist masterpieces, where art captures fleeting moments.",
-    contentImage: "",
+    content: "A masterpiece in progress.",
+    contentImage: "https://i.imgur.com/IUJNBdV.jpg",
     profileImage: "https://i.imgur.com/Rytba1u.png",
     likes: {
       likeCount: 112,
@@ -43,10 +42,9 @@ export const posts = [
   },
 
   {
-    _id: "skdjeo903_kds3_ksd8skd83",
-    content:
-      "Just had an incredible day exploring the world of art! 🎨 Enlarged flowers, desert landscapes—every brushstroke was a journey into new realms of creativity. Share your recent art adventures with me! What have you been exploring in the art world? Let's ignite our imagination together!",
-    contentImage: "",
+    _id: "skdjeo903_kds3_ssd8skd83",
+    content: "Just had an incredible day exploring the world of art!🎨",
+    contentImage: "https://i.imgur.com/mvhL0Lj.jpg",
     profileImage: "https://i.imgur.com/07fk2I9.png",
     likes: {
       likeCount: 82,
@@ -55,7 +53,61 @@ export const posts = [
     },
     comments: [
       {
-        _id: "gYh4lOeD2",
+        _id: "gYh98lOeD2",
+        username: "Noah_Thompson",
+        profileImage: "https://i.imgur.com/Rytba1u.png",
+        text: "Wow! I would definetly go visit myself. Thanks for sharing.",
+        createdAt: "2023-06-01T10:59:01+18:30",
+        votes: {
+          upvotedBy: [
+            { username: "_liam_parker_" },
+            { username: "mikedavis456" },
+            { username: "dwilson77" },
+          ],
+          downvotedBy: [],
+        },
+      },
+    ],
+    fullName: "Liam Parker",
+    username: "_liam_parker_",
+    bio: "Post-Impressionist Painter",
+    createdAt: "2023-06-01T10:55:01+18:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "kdiw8s3_ksd9s_ksd38_ksd2",
+    content:
+      "Diving into the beautiful realm of art, where colors, shapes, and textures intertwine to evoke emotions and spark imagination.",
+    contentImage: "https://i.imgur.com/ob2QI75.jpg",
+    profileImage: "https://i.imgur.com/hRwELzq.png",
+    likes: {
+      likeCount: 65,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    fullName: "Amelia Carter",
+    username: "carterAmii",
+    bio: "Surrealist Painter",
+    createdAt: "2023-06-03T18:20:10+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "skdj5o903_kds3_ksd8skd83",
+    content:
+      "Enlarged flowers, desert landscapes—every brushstroke was a journey into new realms of creativity. Share your recent art adventures with me! What have you been exploring in the art world? Let's ignite our imagination together!",
+    contentImage: "",
+    profileImage: "https://i.imgur.com/07fk2I9.png",
+    likes: {
+      likeCount: 42,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "gYh78lOeD2",
         username: "carterAmii",
         profileImage: "https://i.imgur.com/hRwELzq.png",
         text: "I have been finding inspiration in nature's colors.",
@@ -78,28 +130,11 @@ export const posts = [
   },
 
   {
-    _id: "kdiw8s3_ksd9s_ksd38_ksd2",
+    _id: "ksdop30_2kd3i9_kd3ik3_ks",
     content:
-      "Diving into the realm of abstract art, where colors, shapes, and textures intertwine to evoke emotions and spark imagination.",
-    profileImage: "https://i.imgur.com/hRwELzq.png",
-    likes: {
-      likeCount: 65,
-      likedBy: [],
-      dislikedBy: [],
-    },
-    comments: [],
-    fullName: "Amelia Carter",
-    username: "carterAmii",
-    bio: "Surrealist Painter",
-    createdAt: "2023-06-03T18:20:10+05:30",
-    updatedAt: formatDate(),
-  },
-
-  {
-    _id: "ksdoi30_2kd3i9_kd3ik3_ks",
-    content:
-      "Currently immersed in an art festival bursting with creativity and inspiration! 🎉🎭 From captivating sculptures to thought-provoking installations, I can't get enough. What art festivals or events have caught your attention lately? Let's share our favorite discoveries and keep the artistry flowing!",
-    contentImage: "",
+      "Currently immersed in an art festival bursting with creativity and inspiration!🎉🎭",
+    contentImage:
+      "https://res.cloudinary.com/dla2khf0q/image/upload/v1688303613/social-data/You_Will_Love_These_Beautiful_Animated_Nature_GIFs_by_Mikael_Gustafsson_-_We_Are_Wildness_wxjuzu.gif",
     profileImage: "https://i.imgur.com/dGLRy0M.png",
     likes: {
       likeCount: 48,
@@ -116,8 +151,8 @@ export const posts = [
 
   {
     _id: "sod9s_2k3_ks3ksd8_ksdjw",
-    content: "Expressing emotions through sculpture.",
-    contentImage: "",
+    content: "Expressing emotions through scenery.",
+    contentImage: "https://i.imgur.com/Z70Pii7.jpg",
     profileImage: "https://i.imgur.com/RcLQNaA.png",
     likes: {
       likeCount: 95,
@@ -145,9 +180,69 @@ export const posts = [
   },
 
   {
+    _id: "ksdoi30_2kd3i9_kd3ik3_ks",
+    content:
+      "From captivating sculptures to thought-provoking installations, I can't get enough. What art festivals or events have caught your attention lately? Let's share our favorite discoveries and keep the artistry flowing!",
+    contentImage: "",
+    profileImage: "https://i.imgur.com/dGLRy0M.png",
+    likes: {
+      likeCount: 48,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    fullName: "William Hughes",
+    username: "HugesSiliam",
+    bio: "Cubist Painter, Sculptor",
+    createdAt: "2023-06-08T09:10:20+08:00",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "ksd2k3_ksdo29_2ksd_kccj2",
+    content:
+      "Immersing in the dreamlike landscapes and ethereal brushstrokes of impressionist masterpieces",
+    contentImage: "https://i.imgur.com/i6FhLM4.jpg",
+    profileImage: "https://i.imgur.com/Rytba1u.png",
+    likes: {
+      likeCount: 52,
+      likedBy: [
+        {
+          fullName: "William Hughes",
+          username: "HugesSiliam",
+          profileImage: "https://i.imgur.com/dGLRy0M.png",
+        },
+      ],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "sJd40kE3oP945",
+        profileImage: "https://i.imgur.com/usR4cPn.png",
+        username: "mikedavis456",
+        text: "Same here loving the impressionist masterpieces.",
+        createdAt: "2023-06-06T12:45:30+02:00",
+        votes: {
+          upvotedBy: [
+            { username: "dwilson77" },
+            { username: "carterAmii" },
+            { username: "mikedavis456" },
+          ],
+          downvotedBy: [],
+        },
+      },
+    ],
+    fullName: "Noah Thompson",
+    username: "Noah_Thompson",
+    bio: "Renaissance Polymath",
+    createdAt: "2023-06-05T12:45:30+02:00",
+    updatedAt: formatDate(),
+  },
+
+  {
     _id: "kdoe9d3k3_k_ksd83k_ksd32",
     content: "Immersed in Nature's Palette, Light, color, fleeting moments.",
-    contentImage: "",
+    contentImage: "https://i.imgur.com/f3kqFYs.jpg",
     profileImage: "https://i.imgur.com/usR4cPn.png",
     likes: {
       likeCount: 20,
@@ -163,10 +258,28 @@ export const posts = [
   },
 
   {
+    _id: "k3_ks3igdd3_ksd86k_ksd72",
+    content: "Capturing fleeting moments!",
+    contentImage:
+      "https://res.cloudinary.com/dla2khf0q/image/upload/v1688303486/social-data/Hidden_Heaven_v01kpd.gif",
+    profileImage: "https://i.imgur.com/3koLVx8.png",
+    likes: {
+      likeCount: 100,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    fullName: "Jenny Felicitas",
+    username: "JoyfulJenny",
+    bio: "Impressionist Painter",
+    createdAt: "2022-09-15T08:30:45+05:00",
+    updatedAt: formatDate(),
+  },
+  {
     _id: "k3_ks3ksdd3_ksd86k_ksd32",
     content:
       "Witnessing the vibrant hues of flowers in full bloom. Nature's color palette is a true work of art!",
-    contentImage: "",
+    contentImage: "https://i.imgur.com/pOJUt9A.jpg",
     profileImage: "https://i.imgur.com/3koLVx8.png",
     likes: {
       likeCount: 60,
