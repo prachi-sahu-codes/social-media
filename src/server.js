1b6dc62db236576caee0fbbd903476bff775d5bd
SahuPrachi <prachisahu998@gmail.com>
2023-07-05 20:37:29 +0530

    feat: post api added

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import {
   likePostHandler,
   dislikePostHandler,
   getAllUserPostsHandler,
+  getAllPostByObserverHandler,
 } from "./backend/controllers/PostController";
 import {
   followUserHandler,
@@ -68,6 +69,7 @@ export function makeServer({ environment = "development" } = {}) {
 
       // post routes (public)
       this.get("/posts", getAllpostsHandler.bind(this));
+      this.get("/posts/:limit/:page", getAllPostByObserverHandler.bind(this));
       this.get("/posts/:postId", getPostHandler.bind(this));
       this.get("/posts/user/:username", getAllUserPostsHandler.bind(this));
 
