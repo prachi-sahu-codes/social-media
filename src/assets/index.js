e8724ab5a1fcaa5c970aacc28fc86a1d02fa3b78
SahuPrachi <prachisahu998@gmail.com>
2023-07-08 16:19:25 +0530

    chore: remove unused png's

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,8 +5,6 @@ export { default as lightGray } from "./icons/lightGray.png";
 export { default as lightActive } from "./icons/lightActive.png";
 export { default as fireGray } from "./icons/fireGray.png";
 export { default as fireActive } from "./icons/fireActive.png";
-export { default as eyeWhite } from "./icons/eyeWhite.png";
-export { default as eyeHideWhite } from "./icons/eyeHideWhite.png";
 
 export { default as bookmarkNow } from "./emptyPage/bookmarkNow.svg";
 export { default as likingNow } from "./emptyPage/likingNow.svg";
